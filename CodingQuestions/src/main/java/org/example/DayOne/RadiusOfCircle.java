50c449a9135a88c7f99f248fb9558b6bde67c44e
rishavbehl <rishavbehl296@gmail.com>
2024-04-25 23:43:14 +0530

    day one code

---
+++ CodingQuestions/src/main/java/org/example/DayOne/RadiusOfCircle.java
@@ -0,0 +1,11 @@
+package org.example.DayOne;
+
+public class RadiusOfCircle {
+    public static void main(String[] args) {
+        int radius = 5;
+        int squareRadius = radius * radius;
+        double area = 3.14 * squareRadius;
+        System.out.println(area);
+
+    }
+}
